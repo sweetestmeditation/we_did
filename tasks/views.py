152a1cda57ebbbb2d60dce9c1f1de6d2f8300954
bngwanforbi <brendan.ngwanforbi@gmail.com>
2023-09-09 09:37:52 -0700

    Implemented views and forms for task operations, user authentication, and added a custom form for task deletion in the views.py file.

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,8 @@
+from django import forms
+
+class DeleteTaskForm(forms.Form):
+    action = forms.ChoiceField(choices=[('Delete', 'Delete'), ('Archive', 'Archive')], widget=forms.RadioSelect)
+
 from typing import Any, Dict
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.list import ListView
@@ -9,8 +14,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
+from django.views.generic.base import View
+from django.shortcuts import redirect, get_object_or_404
 from .models import Task
 from .forms import TaskAttachmentForm
+from django.db import models
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
@@ -95,12 +105,30 @@ class TaskUpdate(UpdateView):
     fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('task-list')  
 
-class TaskDeleteView(DeleteView):  # Renamed to avoid conflict with built-in DeleteView
-    model = Task
-    context_object_name = 'task'
-    success_url = reverse_lazy('task-list')
+class TaskDeleteView(FormView):
+    template_name = 'tasks/task_confirm_delete.html'
+    form_class = DeleteTaskForm  # Use the custom form for the delete/archive choice
 
-def archive_task(request, task_id):
+    def form_valid(self, form):
+        task = Task.objects.get(pk=self.kwargs['pk'])
+        action = form.cleaned_data['action']  # Get the selected action from the form
+        if action == 'Delete':
+            task.delete()
+        elif action == 'Archive':
+            task.archived = True
+            task.save()
+        return HttpResponseRedirect(reverse('task-list'))
+
+class UnarchiveTask(View):
+    template_name = 'tasks/archive_task_list.html'
+    fields = '__all__'
+
+class TaskView(models.Model):
+    # Your other fields
+    archived = models.BooleanField(default=False)
+
+
+"""def archive_task(request, task_id):
     task = get_object_or_404(Task, pk=task_id)
     task.archived = True
     task.save()
@@ -120,9 +148,25 @@ def delete_task(request, task_id):
     task.save()
     
     return redirect('task-list')
-
+"""
 def task_list(request):
     # Only fetch tasks that are not archived
     tasks = Task.objects.filter(archived=False)
     context = {'tasks': tasks}
     return render(request, 'task_list.html', context)
+
+class ArchiveTask(View):
+    def post(self, request, task_id):
+        task = get_object_or_404(Task, pk=task_id)
+        task.archived = True
+        task.save()
+        return redirect('task-list')  # Redirect to your task list view
+    
+class ArchiveTaskList(ListView):
+    model = Task
+    template_name = 'archive_task_list.html'
+    context_object_name = 'archived_tasks'
+
+    def get_queryset(self):
+        return Task.objects.filter(archived=True)
+
